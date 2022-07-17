4761ecb75a943c8e9423f80490859f1d8e3a4d44
davidhudman <12885667+davidhudman@users.noreply.github.com>
2022-07-16 20:51:17 -0400

    show individual article pages

--- web/src/components/ArticleCell/ArticleCell.js
+++ web/src/components/ArticleCell/ArticleCell.js
@@ -1,7 +1,10 @@
 export const QUERY = gql`
   query FindArticleQuery($id: Int!) {
-    article: article(id: $id) {
+    article: post(id: $id) {
       id
+      title
+      body
+      createdAt
     }
   }
 `
@@ -15,5 +18,13 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ article }) => {
-  return <div>{JSON.stringify(article)}</div>
+  return (
+    <article key={article.id}>
+      <header>
+        <h2>{article.title}</h2>
+      </header>
+      <p>{article.body}</p>
+      <div>Posted at: {article.createdAt}</div>
+    </article>
+  )
 }
--- web/src/pages/ArticlePage/ArticlePage.js
+++ web/src/pages/ArticlePage/ArticlePage.js
@@ -1,16 +1,13 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+
+import ArticleCell from 'src/components/ArticleCell'
 
 const ArticlePage = ({ id }) => {
   return (
     <>
       <MetaTags title="Article" description="Article page" />
 
-      <h1>ArticlePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ArticlePage/ArticlePage.js</code>
-      </p>
-      <p>My id is {id}</p>
+      <ArticleCell id={id} />
     </>
   )
 }
