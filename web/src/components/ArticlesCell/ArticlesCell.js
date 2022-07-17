9ba51330d3eeb83b1a499d14a9cea82eb421292d
davidhudman <12885667+davidhudman@users.noreply.github.com>
2022-07-16 20:28:45 -0400

    add new articles cells to homepage

--- web/src/components/ArticlesCell/ArticlesCell.js
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -1,7 +1,10 @@
 export const QUERY = gql`
   query ArticlesQuery {
-    articles {
+    articles: posts {
       id
+      title
+      body
+      createdAt
     }
   }
 `
@@ -15,11 +18,13 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ articles }) => {
-  return (
-    <ul>
-      {articles.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
-      })}
-    </ul>
-  )
+  return articles.map((article) => (
+    <article key={article.id}>
+      <header>
+        <h2>{article.title}</h2>
+      </header>
+      <p>{article.body}</p>
+      <div>Posted at: {article.createdAt}</div>
+    </article>
+  ))
 }
